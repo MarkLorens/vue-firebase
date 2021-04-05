843260b687dbf8fb245f38f82e86eab982ec2339
Mark Lorens <57747268+MarkLorens@users.noreply.github.com>
2021-04-05 09:49:15 +0800

    Housekeeping and About
    1. Logout is now in a dropdown under current user
    2. Added an empty About page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,13 @@
 <template>
     <ul>
-        <li><a href="#">Home?</a></li>
-        <li><a href="#">Somewhere maybe</a></li>
-        <li style="float:right"><a @click="Logout">Logout</a></li>
-        <li style="float:right"><a href="#">{{ user }}</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About Me</a></li>
+        <li class="dropdown" style="float:right">
+          <a href="javascript:void(0)" class="dropbtn">{{ user }} <i class="arrow down"></i> </a>
+          <div class="dropdown-content">
+            <a @click="Logout">Logout</a>
+          </div>
+      </li>
     </ul>
 </template>
 
@@ -37,22 +41,54 @@ ul {
   overflow: hidden;
   background-color: rgb(44, 41, 219);
 }
-
 li {
   float: left;
 }
-
-li a {
+li a{
   display: block;
   color: white;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
 }
-
-/* Change the link color to #111 (black) on hover */
 li a:hover {
   background-color: #111;
   cursor: pointer;
+}
+li.dropdown {
+  display: inline-block;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background: #eee;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+.dropdown-content a {
+  color: #111;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+.dropdown-content a:hover{
+  background: #111;
+  color: white;
+}
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+.arrow {
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+  margin: 2px;
+}
+.down {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
 }
 </style>
